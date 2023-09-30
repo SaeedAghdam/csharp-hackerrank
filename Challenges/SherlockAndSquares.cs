3b5c8e7a4ccc270eb0c4b80eac372d28c8fd4b33
Saeed Aghdam <saeed.snfa@hotmail.com>
2023-09-30 17:32:58 -0400

    Added Sherlock and squares solution

---
+++ Challenges/SherlockAndSquares.cs
@@ -0,0 +1,31 @@
+﻿namespace HackerRank.Challenges
+{
+    //https://www.hackerrank.com/challenges/sherlock-and-squares
+    public static class SherlockAndSquares
+    {
+        public static void Run()
+        {
+            Console.WriteLine(squares(17, 17));
+        }
+
+        public static int squares(int a, int b)
+        {
+            var counter = 0;
+
+            if (a > b)
+                return counter;
+
+            var sqrtA = Math.Sqrt(a);
+            var sqrtB = Math.Sqrt(b);
+
+            for (int i = (int)sqrtA; i <= (int)sqrtB; i++)
+            {
+                var _i = i * i;
+                if (_i >= a && a <= b)
+                    counter++;
+            }
+
+            return counter;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Solutions for the following challenegs are uploaded:
 - [**Lego Block**](https://github.com/SaeedAghdam/csharp-hackerrank/blob/main/Challenges/LegoBlocks.cs)
 - [**NewKingOrder**](https://github.com/SaeedAghdam/csharp-hackerrank/blob/main/Challenges/KingOrder.cs)
 - [**JesseAndCookies**](https://github.com/SaeedAghdam/csharp-hackerrank/blob/main/Challenges/JesseAndCookies.cs)
+- [**SherlockAndSquares**](https://github.com/SaeedAghdam/csharp-hackerrank/blob/main/Challenges/SherlockAndSquares.cs)
